006d520ff6e5ad440afd2c10e6416027ddf00899
abindacarmo <abindacarmo08@gmail.com>
2026-09-10 05:11:54 +0900

    Add frontend templates and views for all store pages

--- store/views.py
+++ store/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Category
 
-# Create your views here.
+def home(request):
+    return render(request, 'store/home.html')
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'store/category_list.html', {'categories': categories})
+
+def product_list(request):
+    return render(request, 'store/product_list.html')
+
+def transaction_list(request):
+    return render(request, 'store/transaction_list.html')
+
+def expense_list(request):
+    return render(request, 'store/expense_list.html')
