8e973180de6787188d38e12487e9526e407c2d68
abindacarmo <abindacarmo08@gmail.com>
2026-09-10 14:53:05 +0900

    Implement Category and Product creation forms, models, and views

--- store/models.py
+++ store/models.py
@@ -5,3 +5,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
